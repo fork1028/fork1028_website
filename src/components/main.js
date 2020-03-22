092e2c130feb00d003d6c33564f559cca71396d9
fork1028 <xichen_@live.cn>
2020-03-22 01:35:49 -0400

    update

--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Homepage} />
+    <Route exact path="/fork1028_website" component={Homepage} />
     <Route path="/about" component={About} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
