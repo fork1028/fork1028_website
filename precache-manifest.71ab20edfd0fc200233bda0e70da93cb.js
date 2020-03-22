502a48c913dd2e3a7726b203af30be93e035807b
fork1028 <xichen_@live.cn>
2020-03-22 01:15:42 -0400

    Updates

---
+++ precache-manifest.71ab20edfd0fc200233bda0e70da93cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501c94cad5d893d3072be407ba15d369",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c95e8c30549dae20b189",
+    "url": "./static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "8bbe96a31df4b2e64a2d",
+    "url": "./static/css/main.37a39c02.chunk.css"
+  },
+  {
+    "revision": "c95e8c30549dae20b189",
+    "url": "./static/js/2.cc8fc60b.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "./static/js/2.cc8fc60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbe96a31df4b2e64a2d",
+    "url": "./static/js/main.3b4038d4.chunk.js"
+  },
+  {
+    "revision": "1b340271036c0b3dbb0c",
+    "url": "./static/js/runtime-main.b4b2b369.js"
+  }
+]);
